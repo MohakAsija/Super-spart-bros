b51d034e6a2448d66abd139781e28c9a5777171c
CIAIP21JUL5001 <2019uch0020@iitjammu.ac.in>
2022-06-16 22:52:46 +0530

    Supers Sparty Bros

---
+++ Assets/Scripts/Addlife.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Addlife : MonoBehaviour {
+	public bool taken = false;
+	// if the player touches the coin, it has not already been taken, and the player can move (not dead or victory)
+	// then take the coin
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if ((other.tag == "Player" ) && (!taken) && (other.gameObject.GetComponent<CharacterController2D>().playerCanMove))
+		{
+			// mark as taken so doesn't get taken multiple times
+			taken=true;
+			// do the player collect coin thing
+			GameManager.gm.addlive();
+			other.gameObject.GetComponent<CharacterController2D> ().extralife ();
+			DestroyObject (gameObject);
+
+		}
+	}
+
+}
